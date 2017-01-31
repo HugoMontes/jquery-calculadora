e0b3dd83346379d8dda5ba2ad84b57716efc60cc
Felix Montes <hugomontes927@gmail.com>
2017-01-31 11:54:27 -0400

    Funcionalidad sumar, restar, multiplicar y dividir

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,12 +1,49 @@
-var display=$('#txtDisplay'), resultado;
+var display=$('#txtDisplay'), resultado=0, operador='';
 $(document).ready(function(){
-	$('#txtDisplay').on('input',function(){
-		console.log("cambio...");
-	});
 	$('.btn-numero').click(function(){
+		if(operador=='+' || operador=='-' || operador=='*' || operador=='/' || operador=='='){
+			display.val('');
+			//operador='';	
+		}
 		display.val(display.val()+$(this).text());
 	});
 	$('#btn-suma').click(function(){
-		resultado=display;
+		operador='+';
+		resultado+=parseInt(display.val());
+		display.val(resultado);
+	});
+	$('#btn-resta').click(function(){
+		operador='-';
+		if(resultado==0){
+			resultado=parseInt(display.val());
+		}else{
+			resultado-=parseInt(display.val());
+		}
+		display.val(resultado);
+	});
+	$('#btn-multiplicar').click(function(){
+		operador='*';
+		resultado=1;
+		resultado*=parseInt(display.val());
+		display.val(resultado);
+	});
+	$('#btn-division').click(function(){
+		operador='/';
+		resultado/=parseInt(display.val());
+		display.val(resultado);
+	});
+	$('#btn-igual').click(function(){
+		if(operador=='+'){
+			resultado+=parseInt(display.val());	
+		}else if(operador=='-'){
+			resultado-=parseInt(display.val());
+		}else if(operador=='*'){
+			resultado*=parseInt(display.val());
+		}else if(operador=='/'){
+			resultado/=parseInt(display.val());
+		}		
+		display.val(resultado);
+		resultado=0;
+		operador='=';
 	});
 });
